b1813bad7668cd9c0800561f3dd6c2ff33f4dce9
Zack McCauley <wpgraphix@gmail.com>
2018-07-13 08:39:41 -0600

    First test of Vernier

---
+++ Vernier/VernierURLProvider.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+"""
+Vernier Software YML to URL Provider
+"""
+
+import yaml
+import urllib2
+import urlparse
+
+from autopkglib import Processor, ProcessorError
+
+__all__ = "VernierURLProvider"
+
+class VernierURLProvider(Processor):
+    description = "Converts Vernier yaml release to a URL"
+    input_variables = {
+        "apptype" : {
+            "required": True,
+            "description": "Determins which app to download the yml to convert to a release url. Use ga for Graphical Analysis and sa for Spectral Analysis"
+            }
+        }
+    output_variables = {
+        "url": {
+            "description": "The full url for the file you want to download."
+        },
+        "version": {
+            "description": "The version of the support download"
+        }
+    }
+
+    def main(self):
+        apptype = self.env["apptype"]
+        self.output("App choosen is: {}".format(self.env["apptype"]))
+        header = {'User-Agent': 'Mozilla/5.0'}
+        url = "http://software-releases.graphicalanalysis.com/{0}/mac/release/release-mac.yml".format(apptype)
+        req = urllib2.Request(url, headers=header)
+        file = urllib2.urlopen(req)
+        yamlfile = yaml.load(file)
+        dmgpath = yamlfile["path"].rsplit(".", 1)[0] + ".dmg"
+        fileurl = urlparse.urljoin(url, dmgpath)
+        self.output("DMG URL is {}".format(fileurl))
+        self.env["url"] = fileurl
+        self.output("Version is {}".format(yamlfile["version"]))
+        self.env["version"] = yamlfile["version"]
+
+
+if __name__ == "__main__":
+    PROCESSOR = VernierURLProvider()
+    PROCESSOR.execute_shell()
